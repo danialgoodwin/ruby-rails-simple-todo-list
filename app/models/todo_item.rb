624ffe756e8019deb10fe3ab1733e41563cfde2f
Danial Goodwin <danialgoodwin@gmail.com>
2015-02-05 22:47:26 -0500

    Update layout

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -4,6 +4,11 @@ class TodoItem < ActiveRecord::Base
   validates :content, presence: true,
                       length: { minimum: 2 }
 
+  # Two different ways of writing basically same thing,
+  # in the later one, rails will write in the style of the first.
+  scope :complete, -> { where("completed_at is not null") }
+  scope :incomplete, -> { where(completed_at: nil) }
+
   def complete?
     !completed_at.blank?
   end
