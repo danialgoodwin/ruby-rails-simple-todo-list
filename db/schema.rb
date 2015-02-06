df6fcb7c2b1b3d99547acae18d65260c9f6b6165
Danial Goodwin <danialgoodwin@gmail.com>
2015-02-05 20:29:55 -0500

    Mark todo item as complete

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205061806) do
+ActiveRecord::Schema.define(version: 20150206011101) do
 
   create_table "todo_items", force: :cascade do |t|
     t.integer  "todo_list_id"
     t.string   "content"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.datetime "completed_at"
   end
 
   add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
